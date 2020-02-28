c497750e131f573a4e694ad84626e10af616d5b5
George Sun <Gsunems@gmail.com>
2020-02-27 22:58:46 -0600

    adding my folders

--- Develop/lib/app.js
+++ Develop/lib/app.js
@@ -1,19 +1,185 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("/Manager");
+const Engineer = require("/Engineer");
+const Intern = require("/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
-​
+const fs = require("fs");​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
+const outputPath = path.join(OUTPUT_DIR, "team.html");​
+const render = require("htmlRenderer");​​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 ​
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const questions = [{
+        type: "input",
+        name: "myGithub",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "myProject",
+        message: "What is the name of your Project?"
+    },
+    {
+        type: "input",
+        name: "myDescription",
+        message: "Give a description of your Project"
+    },
+
+    {
+        type: "input",
+        name: "myInstall",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "myInstructions",
+        message: "Provide instructions and examples for use"
+    },
+    {
+        type: "input",
+        name: "myCollabs",
+        message: "List collaborators and third-party assets"
+    },
+    {
+        type: "list",
+        name: "myLicense",
+        message: "What license did you use?",
+        choices: ["MIT", "BSD", "GNU"]
+    },
+    {
+        type: "input",
+        name: "myTests",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "myContribution",
+        message: "What does the user need to know about contributing to the repo?"
+    }
+
+];
+
+
+
+function promptUser() {
+    return inquirer.prompt(questions);
+};
+
+function generateREADME(answer, image, banner) {
+    return `# ${answer.myProject}
+##    
+${banner}
+## Description 
+    
+${answer.myDescription}
+    
+## Table of Contents
+    
+    
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+    
+    
+## Installation
+    
+${answer.myInstall}
+    
+## Usage 
+    
+${answer.myInstructions}
+    
+## Credits
+    
+${answer.myCollabs}
+    
+## License
+    
+${answer.myLicense}
+## Tests
+${answer.myTests}
+## Contributing
+${answer.myContribution}
+![My Avatar](${image})
+`
+}
+// ![My Avatar] (${avatar_url})
+
+
+
+
+async function getImage(username) {
+    try {
+        const queryUrl = "https://api.github.com/users/" + username;
+
+        const response = await axios.get(queryUrl);
+        const avatar_url = await response.data.avatar_url;
+
+        // console.log(avatar_url);
+
+        return avatar_url
+
+    } catch (error) {
+        console.error(error);
+    }
+
+
+};
+async function getBadge(license) {
+    try {
+        if (license === "MIT") {
+            return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        };
+        if (license === "BSD") {
+            return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+        };
+
+        if (license === "GNU") {
+            return "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)"
+        };
+
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+async function init() {
+    console.log("Fill in the prompts to create the readme file:");
+
+    try {
+        const answers = await promptUser();
+
+        const username = answers.myGithub;
+
+        const image = await getImage(username);
+
+        const license = answers.myLicense;
+
+        const banner = await getBadge(license);
+
+        const md = generateREADME(answers, image, banner);
+
+
+
+
+        await writeFileAsync("goodREADME.md", md);
+
+        console.log("Successfully wrote to README.md");
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+init();
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -1,66 +1,94 @@
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const util = require("util");
+
+
+const Manager = require("./constructors/Manager");
+const Engineer = require("./constructors/Engineer");
+const Intern = require("./constructors/Intern");
+
 
 const templatesDir = path.resolve(__dirname, "../templates");
 
-const render = employees => {
-  const html = [];
+const buildDir = path.resolve(__dirname, "../build/");
 
-  html.push(employees
-    .filter(employee => employee.getRole() === "Manager")
-    .map(manager => renderManager(manager))
-  );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Engineer")
-    .map(engineer => renderEngineer(engineer))
-  );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Intern")
-    .map(intern => renderIntern(intern))
-  );
 
-  return renderMain(html.join(""));
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
 
-};
 
-const renderManager = manager => {
-  let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
-  template = replacePlaceholders(template, "name", manager.getName());
-  template = replacePlaceholders(template, "role", manager.getRole());
-  template = replacePlaceholders(template, "email", manager.getEmail());
-  template = replacePlaceholders(template, "id", manager.getId());
-  template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
-  return template;
-};
 
-const renderEngineer = engineer => {
-  let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
-  template = replacePlaceholders(template, "name", engineer.getName());
-  template = replacePlaceholders(template, "role", engineer.getRole());
-  template = replacePlaceholders(template, "email", engineer.getEmail());
-  template = replacePlaceholders(template, "id", engineer.getId());
-  template = replacePlaceholders(template, "github", engineer.getGithub());
-  return template;
-};
+async function render(employees) {
+    const html = [];
 
-const renderIntern = intern => {
-  let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
-  template = replacePlaceholders(template, "name", intern.getName());
-  template = replacePlaceholders(template, "role", intern.getRole());
-  template = replacePlaceholders(template, "email", intern.getEmail());
-  template = replacePlaceholders(template, "id", intern.getId());
-  template = replacePlaceholders(template, "school", intern.getSchool());
-  return template;
-};
+    const [
+        managerTemplate,
+        internTemplate,
+        engineerTemplate,
+        mainTemplate
+    ] = await Promise.all([
+        readFile(path.resolve(templatesDir, "manager.html"), "utf8"),
+        readFile(path.resolve(templatesDir, "intern.html"), "utf8"),
+        readFile(path.resolve(templatesDir, "engineer.html"), "utf8"),
+        readFile(path.resolve(templatesDir, "main.html"), "utf8")
+    ]);
 
-const renderMain = html => {
-  const template = fs.readFileSync(path.resolve(templatesDir, "main.html"), "utf8");
-  return replacePlaceholders(template, "team", html);
-};
 
-const replacePlaceholders = (template, placeholder, value) => {
-  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
-  return template.replace(pattern, value);
-};
+    html.push(
+        employees
+        .filter(employee => employee instanceof Manager)
+        .map(employee => {
+            let template = managerTemplate;
+            for (const key in employee) {
+                template = replacePlaceholder(template, key, employee[key]);
+            }
+            return template;
+        })
+        .join("")
+    );
+
+
+    html.push(
+        employees
+        .filter(employee => employee instanceof Engineer)
+        .map(employee => {
+            let template = engineerTemplate;
+            for (const key in employee) {
+                template = replacePlaceholder(template, key, employee[key]);
+            }
+            return template;
+        })
+        .join("")
+    );
+
+
+    html.push(
+        employees
+        .filter(employee => employee instanceof Intern)
+        .map(employee => {
+            let template = internTemplate;
+            for (const key in employee) {
+                template = replacePlaceholder(template, key, employee[key]);
+            }
+            return template;
+        })
+        .join("")
+    );
+
+
+    if (!fs.existsSync(buildDir)) {
+        fs.mkdirSync(buildDir);
+    }
+    await writeFile(
+        path.resolve(buildDir, "index.html"),
+        replacePlaceholder(mainTemplate, "body", html.join(""))
+    );
+}
+
+function replacePlaceholder(template, target, value) {
+    const regex = new RegExp("{{ " + target + " }}", "gm");
+    const newTemplate = template.replace(regex, value);
+    return newTemplate;
+}
 
 module.exports = render;
